c9db44ef573473bd210d8b273ecf0543aa8621e9
Aydin324 <ayddin2004@gmail.com>
2026-02-11 14:12:31 +0100

    Alpha version

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,25 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('valentines');
+  proposalState = signal<'intro' | 'proposal' | 'accepted'>('intro');
+  //noButtonPosition = signal({ x: 0, y: 0 });
+
+  proceed() {
+    if (this.proposalState() === 'intro') {
+      this.proposalState.set('proposal');
+    } else if (this.proposalState() === 'proposal') {
+      this.proposalState.set('accepted');
+    } else {
+      this.proposalState.set('intro');
+    }
+  }
+
+  moveNoButton() {
+    const x = Math.random() * (window.innerWidth - 100);
+    const y = Math.random() * (window.innerHeight - 50);
+    //this.noButtonPosition.set({ x, y });
+  }
 }
