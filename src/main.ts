0aa006448b936364b807670f7bc075bbcb9824f9
Aydin324 <ayddin2004@gmail.com>
2026-02-12 19:53:42 +0100

    Attempt to fix mobile screen sizing

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+function setVh() {
+  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+}
+setVh();
+window.addEventListener('resize', setVh);
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
