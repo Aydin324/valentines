27d07f62df2ced2a93db89b663c9e5d4b834de17
Aydin324 <ayddin2004@gmail.com>
2026-02-13 02:23:00 +0100

    Fixed mobile screen issues

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-function setVh() {
-  document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-}
-setVh();
-window.addEventListener('resize', setVh);
-
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
